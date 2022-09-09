67cda2f1421efe72dc81fcccaa66f75b6cb34785
natassyaldilla <112063379+natassyaldilla@users.noreply.github.com>
2022-09-09 11:12:20 +0700

    Loker

---
+++ tes/resources/views/layouts/master.blade.php
@@ -0,0 +1,9 @@
+@include('layouts.header')
+@include('layouts.navbar')
+@include('layouts.sidebar')
+@include('layouts.content')
+@include('layouts.Footer')
+ <!-- Preloader -->
+ <div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__shake" src="{{asset('AdminLTE-3.2.0')}}/dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+  </div>
